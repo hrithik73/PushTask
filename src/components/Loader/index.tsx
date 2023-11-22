dae5408dbee74fdb7809b05f6f8b881e6be90243
Hritik Singh <hrithiksinghvps_cse18@its.edu.in>
2023-11-22 12:30:55 +0530

    fix: loader style

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,12 +1,16 @@
+import { View } from 'react-native';
 import { ActivityIndicator, MD2Colors } from 'react-native-paper';
+import { useAppTheme } from '~/theme/theme';
+import { makeStyles } from './styles';
 
 const Loader = () => {
+  const theme = useAppTheme();
+  const styles = makeStyles(theme);
+
   return (
-    <ActivityIndicator
-      style={{ margin: 20 }}
-      animating={true}
-      color={MD2Colors.red800}
-    />
+    <View style={styles.container}>
+      <ActivityIndicator animating={true} color={MD2Colors.red800} />
+    </View>
   );
 };
 export default Loader;
---
+++ src/components/Loader/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+import { AppTheme } from '~/theme/theme';
+
+export const makeStyles = (theme: AppTheme) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  });
